387b5fe9e36380ff8a25e355377fb3a771fb3029
Ahmad1Hmasho <a1994h.sy@gmail.com>
2020-06-24 17:28:16 +0200

    Updates

---
+++ precache-manifest.d6b3a31d1b091fe4f361b45b0572674d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501b8a8074fbca952ecfa82d829ee122",
+    "url": "/Bank-konto/index.html"
+  },
+  {
+    "revision": "0ebfb28d66e001cfec2f",
+    "url": "/Bank-konto/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "04e4739aec388c7d11f7",
+    "url": "/Bank-konto/static/js/2.21e645e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bank-konto/static/js/2.21e645e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ebfb28d66e001cfec2f",
+    "url": "/Bank-konto/static/js/main.3fa592e1.chunk.js"
+  },
+  {
+    "revision": "c42d22cb51df47072670",
+    "url": "/Bank-konto/static/js/runtime-main.9294df75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Bank-konto/static/media/logo.5d5d9eef.svg"
+  }
+]);
